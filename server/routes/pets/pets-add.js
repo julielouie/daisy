1b27257f9b7f01bdc0ca05a4c4d3870035535956
Julie Chung <juliehchung96@gmail.com>
2020-03-27 21:15:47 +0000

    Replaced health check and clienterror into main index

--- server/routes/pets/pets-add.js
+++ server/routes/pets/pets-add.js
@@ -1,6 +1,6 @@
 const db = require('../../database');
 
-const petsAdd = (req, res) => {
+const petsAdd = (req, res, next) => {
   const reqProps = ['userId', 'name', 'birthday', 'adoptionDay', 'age', 'breed', 'species', 'coloring', 'allergies', 'diet'];
   for (const prop in reqProps) {
     // eslint-disable-next-line no-prototype-builtins
@@ -13,7 +13,7 @@ const petsAdd = (req, res) => {
     userId, name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet
   } = req.body;
   const userSql = `
-                    SELECT "userId"
+                    SELECT user."userId"
                       FROM user
                      WHERE "userId" = $1;
                   `;
@@ -29,9 +29,11 @@ const petsAdd = (req, res) => {
         res.status(404).send(`userId ${req.body.userId} does not exist`);
       } else {
         db.query(petSql, petParams)
-          .then(result => res.status(200).json(result.rows));
+          .then(result => res.status(200).json(result.rows))
+          .catch(err => next(err));
       }
-    });
+    })
+    .catch(err => next(err));
 };
 
 module.exports = petsAdd;
