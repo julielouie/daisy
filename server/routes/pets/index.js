25eaeda93e95394ce14489851ecaaf1914cebdfe
Julie Chung <juliehchung96@gmail.com>
2020-04-02 19:45:52 +0000

    Added put request to router

--- server/routes/pets/index.js
+++ server/routes/pets/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const petsGet = require('./pets-get');
 const petsAdd = require('./pets-add');
+const petsUpdate = require('./pets-update');
 
 const petsRouter = express.Router();
 
 petsRouter.get('/pets/:userId', petsGet);
 petsRouter.post('/pets', petsAdd);
+petsRouter.put('/pets', petsUpdate);
 
 module.exports = petsRouter;
