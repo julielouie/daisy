69c51c350abeabef0527bf2d7b101d7e826817fe
Julie Chung <juliehchung96@gmail.com>
2020-04-02 20:12:48 +0000

    Working on query for patch request

--- server/routes/pets/pets-update.js
+++ server/routes/pets/pets-update.js
@@ -1,17 +1,27 @@
 const db = require('../../database');
 
 const petsUpdate = (req, res, next) => {
-  const petId = parseInt(req.params.petId);
+  const {
+    name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet, petId
+  } = req.body;
   const sql = `
                 UPDATE pets
-                   SET ("userId", name, birthday, "adoptionDay", age, breed, species, coloring, allergies, diet)
-                 WHERE "petId" = $1
+                   SET name = $1,
+                       birthday = $2,
+                       "adoptionDay" = $3,
+                       age = $4,
+                       breed = $5,
+                       species = $6,
+                       coloring = $7,
+                       allergies = $8,
+                       diet = $9
+                 WHERE "petId" = $10
               `;
-  const params = [petId];
+  const params = [name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet, petId];
   db.query(sql, params)
     .then(result => {
-      if (result.rows.length === 0) {
-        res.status(200).json([]);
+      if (!result.rows.length) {
+        res.status(404).send(`Pet with ID ${req.body.petId} does not exist`);
       } else {
         res.status(200).json(result.rows);
       }
