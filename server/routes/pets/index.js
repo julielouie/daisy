fda42f9acacf5b020eacc66605dd21883523e531
Julie Chung <juliehchung96@gmail.com>
2020-04-06 19:44:19 +0000

    Added delete pet endpoint to router

--- server/routes/pets/index.js
+++ server/routes/pets/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const petsGet = require('./pets-get');
 const petsAdd = require('./pets-add');
 const petsUpdate = require('./pets-update');
+const petsDelete = require('./pets-delete');
 
 const petsRouter = express.Router();
 
 petsRouter.get('/pets/:userId', petsGet);
 petsRouter.post('/pets', petsAdd);
 petsRouter.patch('/pets', petsUpdate);
+petsRouter.delete('/pets', petsDelete);
 
 module.exports = petsRouter;
