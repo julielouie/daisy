8e722d8e9cfc40e7bdcece36ddcb39e92221f850
Julie Chung <juliehchung96@gmail.com>
2020-04-06 20:16:04 +0000

    Revised add pet code for redundant querying

--- server/routes/pets/pets-add.js
+++ server/routes/pets/pets-add.js
@@ -15,27 +15,13 @@ const petsAdd = (req, res, next) => {
   const {
     userId, name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet
   } = req.body;
-  const userSql = `
-                    SELECT "userId"
-                      FROM users
-                     WHERE "userId" = $1;
-                  `;
-  const petSql = `
+  const sql = `
                   INSERT INTO pets ("userId", name, birthday, "adoptionDay", age, breed, species, coloring, allergies, diet)
                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
                  `;
-  const userParams = [userId];
-  const petParams = [userId, name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet];
-  db.query(userSql, userParams)
-    .then(userResult => {
-      if (!userResult.rows.length) {
-        res.status(404).send(`User with ID ${userId} does not exist`);
-      } else {
-        db.query(petSql, petParams)
-          .then(result => res.status(200).json(result.rows))
-          .catch(err => next(err));
-      }
-    })
+  const params = [userId, name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet];
+  db.query(sql, params)
+    .then(result => res.status(200).json(result.rows))
     .catch(err => next(err));
 };
 
