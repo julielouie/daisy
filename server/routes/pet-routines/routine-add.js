2da538bfed9d130b2ccd39a39ecd5d0900cc8b94
Julie Chung <juliehchung96@gmail.com>
2020-04-07 17:40:03 +0000

    Add pet routine endpoint written, to be tested

--- server/routes/pet-routines/routine-add.js
+++ server/routes/pet-routines/routine-add.js
@@ -1,27 +1,30 @@
 const db = require('../../database');
 
 const routinesAdd = (req, res, next) => {
-  const reqProps = ['userId', 'name', 'birthday', 'adoptionDay', 'age', 'breed', 'species', 'coloring', 'allergies', 'diet'];
-  for (const prop in reqProps) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (!req.body.hasOwnProperty(reqProps[prop])) {
-      if (reqProps[prop] === 'adoptionDay' || reqProps[prop] === 'allergies') {
-        continue;
-      }
-      res.status(400).send(`${reqProps[prop]} is required.`);
-      return;
-    }
-  }
   const {
-    userId, name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet
+    petId, routineName, description, isCompleted, isRepeatable
   } = req.body;
+  const petIdSql = `
+                    SELECT "petId"
+                      FROM pets
+                     WHERE "petId" = $1;
+                  `;
   const sql = `
-                  INSERT INTO pets ("userId", name, birthday, "adoptionDay", age, breed, species, coloring, allergies, diet)
-                       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
-                 `;
-  const params = [userId, name, birthday, adoptionDay, age, breed, species, coloring, allergies, diet];
-  db.query(sql, params)
-    .then(result => res.status(200).json(result.rows))
+                INSERT INTO routines ("petId", routineName, description, isCompleted, isRepeatable)
+                      VALUES ($1, $2, $3, $4, $5);
+              `;
+  const petIdParams = [petId];
+  const params = [petId, routineName, description, isCompleted, isRepeatable];
+  db.query(petIdSql, petIdParams)
+    .then(petIdResult => {
+      if (!petIdResult.rows.length) {
+        res.status(404).send(`Pet with ID ${petId} does not exist`);
+      } else {
+        db.query(sql, params)
+          .then(result => res.status(200).json(result.rows))
+          .catch(err => next(err));
+      }
+    })
     .catch(err => next(err));
 };
 
