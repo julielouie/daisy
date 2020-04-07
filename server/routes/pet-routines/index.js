10047acb82d440eece11265ff766343cece47b47
Julie Chung <juliehchung96@gmail.com>
2020-04-07 17:45:42 +0000

    Added pet routines post  request to router

--- server/routes/pet-routines/index.js
+++ server/routes/pet-routines/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const petRoutinesAdd = require('./routine-add');
 
 const petRoutinesRouter = express.Router;
+
+petRoutinesRouter.post('/pet-routines', petRoutinesAdd);
 
 module.exports = petRoutinesRouter;
