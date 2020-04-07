8bd4be22270f220365860b55822e78898814c0bc
Julie Chung <juliehchung96@gmail.com>
2020-04-07 17:07:15 +0000

    Renamed pet info to pet routines

--- server/routes/pet-details/index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-
-const petDetailsRouter = express.Router;
-
-module.exports = petDetailsRouter;
---
+++ server/routes/pet-routines/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const petRoutinesRouter = express.Router;
+
+module.exports = petRoutinesRouter;
