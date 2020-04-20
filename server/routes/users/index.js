a20c46fe7b5c96df6071a844da14c2439aa8ec37
Julie Chung <juliehchung96@gmail.com>
2020-04-20 19:48:49 +0000

    Added user signup to router

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const usersSignup = require('./users-signup');
 
 const usersRouter = express.Router;
+
+usersRouter.post('/users/signup', usersSignup);
 
 module.exports = usersRouter;
