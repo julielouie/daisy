c30d1100bb9809af550d013d920227ade1101196
Julie Chung <juliehchung96@gmail.com>
2020-04-20 22:38:51 +0000

    Completed get user info endpoint

---
+++ server/routes/users/users-get.js
@@ -0,0 +1,22 @@
+const db = require('../../database');
+
+const usersGet = (req, res, next) => {
+  const userId = parseInt(req.params.userId);
+  const sql = `
+               SELECT email, "fullName"
+                 FROM users
+                WHERE "userId" = $1
+              `;
+  const params = [userId];
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows.length) {
+        res.status(200).send(`User with ID ${userId} does not exist`);
+      } else {
+        res.status(200).json(result.rows);
+      }
+    })
+    .catch(err => next(err));
+};
+
+module.exports = usersGet;
