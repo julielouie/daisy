65d377684c8b73c30d6b5eae346e1f52a41c42d2
Julie Chung <juliehchung96@gmail.com>
2020-04-20 21:01:03 +0000

    Starting user login endpoint

---
+++ server/routes/users/users-login.js
@@ -0,0 +1,9 @@
+// const db = require('../../database');
+// const bcrypt = require('bcrypt');
+
+// const usersLogin = (req, res, next) => {
+//   const { email, password } = req.body;
+
+// };
+
+// module.exports = usersLogin;
