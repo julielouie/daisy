d8d7a9a30e4f7c2b08598e3971785ad65c8020ce
Julie Chung <juliehchung96@gmail.com>
2020-04-20 22:33:01 +0000

    Added get user info to router

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const usersGet = require('./users-get');
 const usersSignup = require('./users-signup');
 const usersLogin = require('./users-login');
 const usersUpdate = require('./users-update');
 
 const usersRouter = express.Router();
 
+usersRouter.get('/users/:userId', usersGet);
 usersRouter.post('/users/signup', usersSignup);
 usersRouter.post('/users/login', usersLogin);
 usersRouter.patch('/users', usersUpdate);
