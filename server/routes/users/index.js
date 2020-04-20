36b613c95650f999189bda4636d6e68049b49db1
Julie Chung <juliehchung96@gmail.com>
2020-04-20 22:01:27 +0000

    Added user update to router

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const usersSignup = require('./users-signup');
 const usersLogin = require('./users-login');
+const usersUpdate = require('./users-update');
 
 const usersRouter = express.Router();
 
 usersRouter.post('/users/signup', usersSignup);
 usersRouter.post('/users/login', usersLogin);
+usersRouter.patch('/users', usersUpdate);
 
 module.exports = usersRouter;
