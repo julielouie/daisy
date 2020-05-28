179f6a9a1daddb89ccebf5ada0339fcac6a37e58
Julie Chung <juliehchung96@gmail.com>
2020-05-28 18:24:27 +0000

    Add router to App

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; // Switch, Route
 
-export default class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      user: null,
+      fetchingUser: true
     };
   }
 
@@ -17,9 +18,40 @@ export default class App extends React.Component {
       .finally(() => this.setState({ isLoading: false }));
   }
 
+  fetchUser() {
+    fetch('/api/users')
+      .then(results => results.json())
+      .then(user => {
+        if (user) {
+          this.setState({ user, fetchingUser: false });
+          this.getPoints(user.userId);
+        } else {
+          this.setState({ fetchingUser: false });
+        }
+      })
+      .catch(error => console.error('There was an error:', error.message));
+  }
+
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{ this.state.message.toUpperCase() }</h1>;
+    // return this.state.isLoading
+    //   ? <h1>Testing connections...</h1>
+    //   : <h1>{ this.state.message.toUpperCase() }</h1>;
+    if (this.state.fetchingUser === true) {
+      return null;
+    }
+    return (
+      <>
+        {/* <Header signOut={this.signOut} user={this.state.user} />
+        <Switch>
+          <Route exact path="/" render={props => <DefaultPage {...props}
+            setZip={this.setZip} />} />
+          <Route exact path="/activity-filter" render={props => <ActivityFilter {...props}
+            zip={this.state.zip}
+            setFilter={this.setFilter} />} />
+        </Switch> */}
+      </>
+    );
   }
 }
+
+export default withRouter(App);
