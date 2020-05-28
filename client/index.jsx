7905051c3cf4344135eb31dae049a905ad47a797
Julie Chung <juliehchung96@gmail.com>
2020-05-28 18:16:31 +0000

    Updated index.jsx for router

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from 'react-router-scroll-top';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <ScrollToTop />
+    <App />
+  </Router>,
   document.querySelector('#root')
 );
