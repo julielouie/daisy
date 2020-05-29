5b570838d6b1e6c0f50ecb1c23a38ea9d573f352
Julie Chung <juliehchung96@gmail.com>
2020-05-29 20:42:01 +0000

    Changing background

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -5,12 +5,12 @@ const Home = props => {
     <div className="home container flex-column">
       <div className="d-flex flex-column justify-content-center align-items-center h-50">
         <img src="assets/images/daisy.png" alt="daisy flower" className="logo"/>
-        <h1>daisy</h1>
-        <p>Happy tails make happy tales</p>
+        <h3>daisy</h3>
+        <small>Happy tails make happy tales</small>
       </div>
-      <div className="d-flex flex-column justify-content-center align-items-center h-50">
-        <button>Log In</button>
-        <button>Sign Up</button>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <button className="navi m-4">Log In</button>
+        <button className="navi m-4">Sign Up</button>
       </div>
     </div>
   );
