e25d4c055ba12a6a0a15cdae4ef18ceb8f988a16
Julie Chung <juliehchung96@gmail.com>
2020-05-29 21:30:18 +0000

    Finalized styling

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -5,8 +5,8 @@ const Home = props => {
     <div className="home flex-column">
       <div className="col-md-10 col-lg-8 col-xl-6 m-auto d-flex flex-column justify-content-center align-items-center h-50">
         <img src="assets/images/daisy.png" alt="daisy flower" className="logo"/>
-        <h3>daisy</h3>
-        <small>Happy tails make happy tales</small>
+        <h3 className="mt-3">daisy</h3>
+        <small className="m-2">Happy tails make happy tales</small>
       </div>
       <div className="col-md-10 col-lg-8 col-xl-6 m-auto d-flex flex-column justify-content-center align-items-center">
         <button className="navi m-4">
