01a1ca0b1c6e14b9d0625c40442e72a37320292b
Julie Chung <juliehchung96@gmail.com>
2020-05-29 20:30:04 +0000

    Working through styling

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Home = props => {
   return (
     <div className="home container flex-column">
-      <div className="d-flex flex-column align-items-center">
-        <img src="assets/images/daisy.png" alt="daisy flower"/>
+      <div className="d-flex flex-column justify-content-center align-items-center h-50">
+        <img src="assets/images/daisy.png" alt="daisy flower" className="logo"/>
         <h1>daisy</h1>
         <p>Happy tails make happy tales</p>
       </div>
-      <div className="d-flex flex-column align-items-center">
+      <div className="d-flex flex-column justify-content-center align-items-center h-50">
         <button>Log In</button>
         <button>Sign Up</button>
       </div>
