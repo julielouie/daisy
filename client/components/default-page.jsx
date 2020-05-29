241ca4bc480976028bc629aa09dc9a26d7070438
Julie Chung <juliehchung96@gmail.com>
2020-05-29 19:46:25 +0000

    Altered session data again

--- client/components/default-page.jsx
+++ client/components/default-page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DefaultPage = props => {
   return (
     <div>
-      <div>
+      <div className="">
         <img src="images/daisy.png" alt="daisy flower"/>
         <h1>daisy</h1>
         <p>Happy tails make happy tales</p>
--- server/routes/users/users-get.js
+++ server/routes/users/users-get.js
@@ -1,9 +1,8 @@
 const usersGet = (req, res, next) => {
-  req.session.user = 8;
   if (req.session.user) {
     res.status(200).json(req.session.user);
   } else {
-    res.status(200).send('User must be logged in');
+    res.status(200).send(null);
   }
 };
 
