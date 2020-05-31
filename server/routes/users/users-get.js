a091879809f5ce84b7f16c0627ee8534c89567c1
Julie Chung <juliehchung96@gmail.com>
2020-05-31 19:35:26 +0000

    Hard set temp user

--- server/routes/users/users-get.js
+++ server/routes/users/users-get.js
@@ -1,4 +1,9 @@
 const usersGet = (req, res, next) => {
+  req.session.user = {
+    email: 'j',
+    fullName: 'J J',
+    userId: 8
+  };
   if (req.session.user) {
     res.status(200).json(req.session.user);
   } else {
